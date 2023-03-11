b079b878922d47a6026ab9901877b3e80004a9b0
Gin <279694479@qq.com>
2023-03-11 09:04:11 +0800

    消息增加baseCell

--- GXMessagesTableView/GXMessagesTableView.swift
+++ GXMessagesTableView/GXMessagesTableView.swift
@@ -45,14 +45,12 @@ private extension GXMessagesTableView {
         let lastCell = self.visibleCells.last(where: {$0 is GXMessagesAvatarCellProtocol})
         guard let lastAvatarCell = lastCell as? GXMessagesAvatarCellProtocol else { return }
         guard let lastAvatarIndexPath = self.indexPath(for: lastAvatarCell) else { return }
-        
+
         let previousIndexPath = self.indexPathsForVisibleRows?.last(where: {$0 < lastAvatarIndexPath})
         let lastAvatarData = dataDelegate.gx_tableView(self, avatarDataForRowAt: lastAvatarIndexPath)
-        
-        if self.hoverAvatarData?.gx_senderId != lastAvatarData.gx_senderId ||
-            (self.hoverAvatarData?.gx_messageStatus == lastAvatarData.gx_messageStatus && self.hoverAvatarData?.gx_messageStatus == .sending) {
+
+        if self.hoverAvatarData?.gx_senderId != lastAvatarData.gx_senderId {
             self.gx_resetPreEndAvatar()
-            
             let avatar = lastAvatarCell.createAvatarView()
             dataDelegate.gx_tableView(self, changeForRowAt: lastAvatarIndexPath, avatar: avatar)
             let avatarOrigin = CGPoint(x: lastAvatarCell.avatar.left, y: lastAvatarCell.bottom - lastAvatarCell.avatar.height)
@@ -60,6 +58,22 @@ private extension GXMessagesTableView {
             self.addSubview(avatar)
             self.hoverAvatarData = lastAvatarData
             self.hoverAvatar = avatar
+        }
+        else if (self.hoverAvatarData?.gx_messageStatus == lastAvatarData.gx_messageStatus && self.hoverAvatarData?.gx_messageStatus == .sending) {
+            self.gx_resetPreEndAvatar()
+            let avatar = lastAvatarCell.createAvatarView()
+            dataDelegate.gx_tableView(self, changeForRowAt: lastAvatarIndexPath, avatar: avatar)
+            let avatarHeight = lastAvatarCell.height - lastAvatarCell.avatar.top
+            let avatarOrigin = CGPoint(x: lastAvatarCell.avatar.left, y: lastAvatarCell.bottom - avatarHeight)
+            avatar.frame = CGRect(origin: avatarOrigin, size: lastAvatarCell.avatar.size)
+            self.addSubview(avatar)
+            self.hoverAvatarData = lastAvatarData
+            self.hoverAvatar = avatar
+            
+            if lastAvatarData.gx_messageContinuousStatus == .end || lastAvatarData.gx_messageContinuousStatus == .beginAndEnd {
+                lastAvatarCell.avatar.isHidden = true
+                self.lastHiddenIndexPath = lastAvatarIndexPath
+            }
         }
         else {
             guard let preIndexPath = previousIndexPath else { return }
@@ -73,27 +87,29 @@ private extension GXMessagesTableView {
                 self.gx_resetPreEndAvatar()
                 let hoverAvatar = lastAvatarCell.createAvatarView()
                 dataDelegate.gx_tableView(self, changeForRowAt: lastAvatarIndexPath, avatar: hoverAvatar)
-                let avatarOrigin = CGPoint(x: lastAvatarCell.avatar.left, y: lastAvatarCell.bottom - lastAvatarCell.avatar.height)
+                let avatarHeight = lastAvatarCell.height - lastAvatarCell.avatar.top
+                let avatarOrigin = CGPoint(x: lastAvatarCell.avatar.left, y: lastAvatarCell.bottom - avatarHeight)
                 hoverAvatar.frame = CGRect(origin: avatarOrigin, size: lastAvatarCell.avatar.size)
                 self.addSubview(hoverAvatar)
                 self.hoverAvatarData = lastAvatarData
                 self.hoverAvatar = hoverAvatar
             }
         }
-    
+        
+        guard let avatar = self.hoverAvatar else { return }
+        let avatarHeight = lastAvatarCell.height - lastAvatarCell.avatar.top
         let cellRect = self.rectForRow(at: lastAvatarIndexPath)
         let cellTop = cellRect.minY - self.contentOffset.y
         let cellBottom = cellRect.maxY - self.contentOffset.y
         let tDifference = self.height - cellTop
         let bDifference = self.height - cellBottom
 
-        guard let avatar = self.hoverAvatar else { return }
         if tDifference >= lastAvatarCell.avatar.height {
             if bDifference <= 0 {
-                avatar.top = self.height - avatar.height + self.contentOffset.y
+                avatar.top = self.height - avatarHeight + self.contentOffset.y
             }
             else {
-                avatar.top = cellRect.maxY - avatar.height
+                avatar.top = cellRect.maxY - avatarHeight
             }
         }
         else {
@@ -102,7 +118,7 @@ private extension GXMessagesTableView {
             if self.cellForRow(at: preIndexPath) is GXMessagesAvatarCellProtocol &&
                 (preAvatarData.gx_messageContinuousStatus != .end && preAvatarData.gx_messageContinuousStatus != .beginAndEnd) &&
                 lastAvatarIndexPath.section == preIndexPath.section {
-                avatar.top = self.height - avatar.height + self.contentOffset.y
+                avatar.top = self.height - avatarHeight + self.contentOffset.y
             }
             else {
                 avatar.top = cellRect.minY
@@ -122,3 +138,4 @@ private extension GXMessagesTableView {
     }
     
 }
+
